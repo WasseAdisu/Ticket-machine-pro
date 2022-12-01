bbf8ff383f592c4ce35928bfe10fea01730c4c5c
wa1232 <113332420+wa1232@users.noreply.github.com>
2022-12-01 21:30:47 +0200

    ticket machin files in one file add

---
+++ TW_timeMachine.py
@@ -0,0 +1,56 @@
+cosmotics = 0
+medecine = 0
+perfume = 0
+print("cosmetics")
+print("perfume")
+print("medicine")
+# def darug_store():
+#     if order == "cosmotics":
+#        global cosmotics
+#        cosmotics =cosmotics + 1
+#        print("Your number is : c-",cosmotics )
+#     elif order == "perfume":
+#         global perfume
+#         perfume =perfume + 1
+#         print("Your number is: p -",perfume)
+#     elif order == "medicine":
+#         global medecine
+#         medecine =medecine + 1
+#         print("Your number is: m -",medecine)
+# def message(reminder):
+#     return reminder
+# reminder = message("Please wait until the seller calls you.")
+#
+# print("Well come to our Drug store")
+# list = [123456789,987654321]
+# count = 0
+# count_limit = 3
+# while count_limit > count:
+#     id = int(input("please enter your id: "))
+#     if id in list:
+#         order = input("please choose products name above : " )
+#         while order != "":
+#             darug_store()
+#             per = input("Do you want continue? y/n: ")
+#             if per == "y":
+#                 order = input("please choose products name above: ")
+#             else:
+#                 exit(0)
+
+
+#     count+=1
+# else:
+#     print("Try after a minute")
+#
+# import time
+#
+# my_time = int(input("Enter the time in the seconds: "))
+#
+# for x in range(my_time, 0, -1):
+#     seconds = x % 60
+#     minute = int(x/60) % 60
+#     hours = int(x / 3600)
+#     print(f"{hours:02}:{minute:02}:{seconds:02}")
+#     time.sleep(1)
+#
+# print("Time's up!!")
